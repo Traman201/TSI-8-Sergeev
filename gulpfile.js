223943eca1ea0d4edea1989674dacd9413351a8c
Sergeev <sergeev.rs08@gmail.com>
2020-05-20 18:05:14 +0400

    watch task added

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
-var gulp = require('gulp'),//подключаю модуль gulp
-	less = require('gulp-less'); //подключаю модуль gulp-less для компиляции less
+var gulp = require('gulp'),//подключаю плагин gulp
+	less = require('gulp-less'), //подключаю плагин gulp-less для компиляции less
+	watch = require('gulp-watch');//подключаю плагин gulp-watch
 	
 gulp.task('less', function(){//создаю задачу, которая вызывается при помощи команды gulp less и содержит функцию
 	return gulp.src('app/less/*.less')//получаем источник с помощью gulp.src
 	.pipe(less({}))//Прогоняем полученный файл через gulp-less, получая на выходе css. Прим.: .pipe позволяет выполнять плагины
 	.pipe(gulp.dest('app/css')) //выходные файлы помещаются в папку, указанную в gulp.dest
+});
+
+
+gulp.task('watch', function(){//задача, вызываемая командой gulp watch
+	gulp.watch('app/less/*.less', gulp.series(['tellless']))//метод watch следит за изменениями файлов, которые указаны в качестве первого аргумента и, если находит изменения, вызывает задачу, указанную во втором аргументе
+});
+gulp.task('tellless', function(){//задача вызывается методом watch и выводит на консоль информацию о том, что Less был изменен
+	console.log('Less has been changed')
+	return gulp.src('*.less')
 });
