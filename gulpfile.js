04b0deeccacbea9d3214fbf431b3d90bb77f2049
Sergeev <sergeev.rs08@gmail.com>
2020-05-20 17:07:46 +0400

    styles.css created by compiling Less

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,7 @@ var gulp = require('gulp'),//подключаю модуль gulp
 	less = require('gulp-less'); //подключаю модуль gulp-less для компиляции less
 	
 gulp.task('less', function(){//создаю задачу, которая вызывается при помощи команды gulp less и содержит функцию
-	console.log('hello')
-	return gulp.src('app/less/*.less')
-	.pipe(less({}))
-	.pipe(gulp.dest('app/css')) 
+	return gulp.src('app/less/*.less')//получаем источник с помощью gulp.src
+	.pipe(less({}))//Прогоняем полученный файл через gulp-less, получая на выходе css. Прим.: .pipe позволяет выполнять плагины
+	.pipe(gulp.dest('app/css')) //выходные файлы помещаются в папку, указанную в gulp.dest
 });
